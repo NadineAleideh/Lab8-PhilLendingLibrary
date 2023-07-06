81d923d7cd32f1643048b9b8bf37ab4cebf882f0
Nadine Aleideh <nadinealeideh75@gmail.com>
2023-07-06 11:15:51 +0300

    Add project files.

---
+++ Lab8-PhilLendingLibrary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab8_PhilLendingLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Lab8_PhilLendingLibrary;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
